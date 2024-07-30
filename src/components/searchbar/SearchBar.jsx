a2b5db7905c30a2dc2c103f54addfbfafbb39fdd
SPHLam <samphanhoailam@gmail.com>
2024-07-31 00:28:41 +0700

    Responsive Search Bar

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,5 +1,40 @@
-function SearchBar() {
+import { useState } from "react";
+import "./searchbar.scss";
 
+const types = ["buy", "rent"]
+
+function SearchBar() {
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0
+  })
+
+  function setType(value) {
+    setQuery((previousState) => ({
+      ...previousState,
+      type: value
+    }))
+  }
+
+  return (
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => (
+          <button key={type} onClick={() => (
+            setType(type)
+          )} className={query.type === type ? "active" : ""}>{type}</button>
+        ))}
+      </div>
+      <form action="">
+        <input type="text" name="location" placeholder="City Location"/>
+        <input type="number" name="location" min={0} max={10000000} placeholder="Min Price"/>
+        <input type="number" name="location" min={0} max={10000000} placeholder="Max Price"/>
+        <button><img src="/search.png" alt="" /></button>
+      </form>
+    </div>
+  )
 }
 
 export default SearchBar;
