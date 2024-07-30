5325318025d3245283c698aff96ca5e24daf7b14
SPHLam <samphanhoailam@gmail.com>
2024-07-30 18:46:37 +0700

    Responsive Nav Bar (hamburger menu on small devices)

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,13 @@
 import "./navbar.scss"
+import {useState} from 'react'
 
 function Navbar() {
+  const [hamburgerButton, setHamburgerButton] = useState(false);
+
+  function setOpen() {
+    setHamburgerButton(!hamburgerButton)
+  }
+
   return (
     <nav>
       <div className="left">
@@ -16,6 +23,17 @@ function Navbar() {
       <div className="right">
         <a href="">Sign In</a>
         <a href="" className="register">Sign Up</a>
+        <div className="menuIcon" onClick={setOpen}>
+          <img src="/menu.png" alt="" />
+        </div>
+        <div className={hamburgerButton ? "menu active" : "menu"}>
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Contact</a>
+          <a href="">Agents</a>
+          <a href="">Sign In</a>
+          <a href="">Sign Up</a>
+        </div>
       </div>
     </nav>
   )
