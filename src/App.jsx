131212e583f0287b04053059b0fe10742c751f26
SPHLam <samphanhoailam@gmail.com>
2024-07-31 21:57:35 +0700

    Routing using React Router DOM & React Router DOM Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import "./layout.scss"
-import Navbar from "./components/navbar/Navbar"
 import HomePage from "./routes/HomePage/homePage"
+import ListPage from "./routes/ListPage/listPage"
+import Layout from "./routes/layout/layout"
+import LoginPage from "./routes/LoginPage/LoginPage"
+import SinglePage from "./routes/SinglePage/SinglePage"
+import {
+  createBrowserRouter, 
+  RouterProvider,
+} from "react-router-dom"
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/", 
+          element: <HomePage />
+        },
+        {
+          path: "/list", 
+          element: <ListPage />
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />
+        },
+        {
+          path: "/login",
+          element: <LoginPage />
+        }
+      ]
+    }
+  ])
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
     
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/routes/SinglePage/SinglePage.jsx
@@ -0,0 +1,9 @@
+import "./singlePage.scss";
+
+function SinglePage() {
+  return (
+    <div className="singlePage">Single</div>
+  )
+}
+
+export default SinglePage;
