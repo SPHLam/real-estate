e45d7f76101bc5655ed5ae63814bf247fba16bac
SPHLam <samphanhoailam@gmail.com>
2024-08-03 18:05:20 +0700

    Image Slider

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react";
 import "./slider.scss";
 
-function Slider() {
+function Slider(props) {
+  const [imgIndex, setImgIndex] = useState(null);
+
+  function changeSlide(direction) {
+    if (direction === "left") 
+      setImgIndex(imgIndex > 0 ? imgIndex - 1 : props.images.length - 1)
+    else 
+      setImgIndex(imgIndex < props.images.length - 1 ? imgIndex + 1 : 0)
+  }
+
   return (
-    <div className="slider">Slider</div>
+    <div className="slider">
+      {imgIndex !== null && (<div className="fullSlider">
+        <div className="arrow">
+          <img src="/arrow.png" alt="" onClick={() => changeSlide("left")}/>
+        </div>
+        <div className="imgContainer">
+          <img src={props.images[imgIndex]} alt="" />
+        </div>
+        <div className="arrow">
+          <img src="/arrow.png" className="right" alt="" onClick={() => changeSlide("right")}/>
+        </div>
+        <div className="close" onClick={() => setImgIndex(null)}>X</div>
+      </div>)}
+      <div className="bigImage">
+        <img src={props.images[0]} alt="" onClick={() => setImgIndex(0)}/>
+      </div>
+      <div className="smallImages">
+        {props.images.slice(1).map((image, index) => (
+          <img key={index} src={image} alt="" onClick={() => setImgIndex(index + 1)} />
+        ))}
+      </div>
+    </div>
   )
 }
 
